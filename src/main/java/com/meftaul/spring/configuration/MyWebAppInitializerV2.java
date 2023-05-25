c9bb45baf3755eb5726bf99a7a6b51e9cedb9298
meftaul <meftaulhaque@gmail.com>
2023-05-25 23:19:22 +0600

    Step-08: Configure dispatcher servlet v2

---
+++ src/main/java/com/meftaul/spring/configuration/MyWebAppInitializerV2.java
@@ -0,0 +1,22 @@
+package com.meftaul.spring.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebAppInitializerV2 extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        Class arr[] = {AppConfig.class};
+        return arr;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        String arr[] = { "/api/*" };
+        return arr;
+    }
+}
--- src/main/java/com/meftaul/spring/configuration/MyWebApplicationInitializer.java
+++ src/main/java/com/meftaul/spring/configuration/MyWebApplicationInitializer.java
@@ -7,24 +7,20 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-public class MyWebApplicationInitializer implements WebApplicationInitializer {
-    @Override
+public class MyWebApplicationInitializer /*implements WebApplicationInitializer*/ {
+    /*@Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         System.out.println("======================");
         System.out.println("My Application initializing...");
 
-        // Create aplication context IOC container
         AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
         webApplicationContext.register(AppConfig.class);
 
-        // create a dispatcher servlet
         DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
 
-        // register the dispatcher servlet with the servlet context object
         ServletRegistration.Dynamic customServletContext =
                 servletContext.addServlet("myDispatcherServlet", dispatcherServlet);
-
         customServletContext.setLoadOnStartup(1);
         customServletContext.addMapping("/api/*");
-    }
+    }*/
 }
